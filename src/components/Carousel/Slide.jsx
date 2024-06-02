e3399215783cd225383dbbbae281f56e2b222bb5
Alamin4D <alaminahmed79000@gmail.com>
2024-06-02 16:55:44 +0600

    added the navbar section

---
+++ src/components/Carousel/Slide.jsx
@@ -0,0 +1,29 @@
+
+
+const Slide = ({ image }) => {
+    return (
+        <div
+            className='w-full bg-center bg-cover h-[400px] lg:h-[800px] relative'
+            style={{
+                backgroundImage: `url(${image})`,
+            }}
+        >
+            <div className='flex items-center justify-center w-full h-full bg-gray-900/40'>
+                <div className='font-lato left-10 lg:left-20 lg:top-50 absolute'>
+                    <h1 className='lg:text-6xl text-4xl font-bold text-white'>
+                        Welcome To <br /> Eduan University
+                    </h1>
+                    <br />
+                    <p className='text-lg lg:w-2/3 lg:text-[22px] text-white'>Deleniti nostrum laboriosam praesentium quasi quam voluptate.
+                        Frycimus suscipit</p>
+                    <div>
+                        <button className="btn px-6 py-3 text-lg bg-red-800 text-white font-semibold mt-8">Apply Now</button>
+                        <button className="btn px-6 py-3 text-lg bg-yellow-500 font-semibold ml-3">Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Slide
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+// import { Helmet } from 'react-helmet-async'
+// import Categories from '../../components/Categories/Categories'
+// import Rooms from '../../components/Home/Rooms'
+
+import Carousel from "../../components/Carousel/Carousel"
+
+const Home = () => {
+    return (
+        <div>
+            <Carousel />
+        </div>
+    )
+}
+
+export default Home
